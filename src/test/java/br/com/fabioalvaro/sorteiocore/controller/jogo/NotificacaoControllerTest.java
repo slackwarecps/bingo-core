3c517aa1f958bf40f4e9032ff95fbf8b8e528719
FABIO ALVARO PEREIRA <fabio.alvaro@portoseguro.com.br>
2024-06-30 08:17:44 -0300

    Notificacao implementada

---
+++ src/test/java/br/com/fabioalvaro/sorteiocore/controller/jogo/NotificacaoControllerTest.java
@@ -0,0 +1,22 @@
+package br.com.fabioalvaro.sorteiocore.controller.jogo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class NotificacaoControllerTest {
+    @Test
+    void testCreateNotificacao() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    void testGetNotificacoesByJogadorId() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    void testUpdateNotificacao() {
+        assertEquals(true, true);
+    }
+}
---
+++ src/test/java/br/com/fabioalvaro/sorteiocore/service/NotificacaoServiceTest.java
@@ -0,0 +1,47 @@
+package br.com.fabioalvaro.sorteiocore.service;
+
+import org.junit.jupiter.api.Test;
+
+public class NotificacaoServiceTest {
+    @Test
+    void testFindByJogadorId() {
+        // // Arrange
+        // String jogadorId = "123";
+        // List<Notificacao> expectedNotificacoes = new ArrayList<>();
+        // // Add expected notificacoes to the list
+        // Notificacao notificacao = new Notificacao();
+        // notificacao.setId("1234");
+        // expectedNotificacoes.add(notificacao);
+
+        // // Mock the notificacaoRepository
+        // NotificacaoRepository notificacaoRepository =
+        // Mockito.mock(NotificacaoRepository.class);
+        // Mockito.when(notificacaoRepository.findByJogadorId(jogadorId)).thenReturn(expectedNotificacoes);
+
+        // // Create an instance of NotificacaoService and set the mocked repository
+        // NotificacaoService notificacaoService = new NotificacaoService();
+        // notificacaoService.setNotificacaoRepository(notificacaoRepository);
+
+        // // Act
+        // List<Notificacao> actualNotificacoes =
+        // notificacaoService.findByJogadorId(jogadorId);
+
+        // // Assert
+        // assertEquals(expectedNotificacoes, actualNotificacoes);
+    }
+
+    @Test
+    void testNotificaCartela() {
+
+    }
+
+    @Test
+    void testSave() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
