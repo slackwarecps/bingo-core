91fa60e68e8fb01da7e865e5ecbc538a09d43f9a
fabiopereiraz <fabio.pereira@icarros.com.br>
2024-07-21 10:51:20 -0300

    chore: Update pom.xml to use Spring Boot 2.5.4

--- src/main/java/br/com/fabioalvaro/sorteiocore/controller/HealthController.java
+++ src/main/java/br/com/fabioalvaro/sorteiocore/controller/HealthController.java
@@ -1,27 +1,21 @@
 package br.com.fabioalvaro.sorteiocore.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.fabioalvaro.sorteiocore.utils.VersionReader;
 
 @RestController
 @RequestMapping("/sorteio-core/v1")
 public class HealthController {
 
 
-        @Autowired
-    private VersionReader versionReader;
 
 
     @GetMapping("/health")
     public ResponseEntity<String> healthPadrao() {
-        String resposta = "sorteio-core: OK - ";
-        String version = versionReader.getVersion();
-        resposta = resposta + version;
+        String resposta = "sorteio-core: OK";
         return ResponseEntity.ok(resposta);
     }
 }
