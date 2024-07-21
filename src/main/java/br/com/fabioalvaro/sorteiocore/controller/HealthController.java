a2e20dfbf20c34dd4df68778392acc8a07de1732
fabiopereiraz <fabio.pereira@icarros.com.br>
2024-07-21 10:10:36 -0300

    chore: Update HealthController to include version information

--- src/main/java/br/com/fabioalvaro/sorteiocore/controller/HealthController.java
+++ src/main/java/br/com/fabioalvaro/sorteiocore/controller/HealthController.java
@@ -1,17 +1,27 @@
 package br.com.fabioalvaro.sorteiocore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fabioalvaro.sorteiocore.utils.VersionReader;
+
 @RestController
-@RequestMapping("/api/v1/padrao")
+@RequestMapping("/sorteio-core/v1")
 public class HealthController {
+
+
+        @Autowired
+    private VersionReader versionReader;
+
+
     @GetMapping("/health")
     public ResponseEntity<String> healthPadrao() {
-
-        String resposta = "OK Padrao health!!!";
+        String resposta = "sorteio-core: OK - ";
+        String version = versionReader.getVersion();
+        resposta = resposta + version;
         return ResponseEntity.ok(resposta);
     }
 }
